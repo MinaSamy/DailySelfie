c514adffc60460cbb1d10f008f68174f22cbd2af
Mina Samy <m.s.ramzy@gmail.com>
2015-11-14 18:07:54 +0200

    Used weak references

--- app/src/main/java/bloodstone/dailyselfie/android/utils/ImageLoader.java
+++ app/src/main/java/bloodstone/dailyselfie/android/utils/ImageLoader.java
@@ -7,6 +7,7 @@ import android.provider.MediaStore;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,15 +18,20 @@ public class ImageLoader {
     private ExecutorService mExecutorService;
 
     public ImageLoader(){
-        mExecutorService= Executors.newFixedThreadPool(5);
+        mExecutorService= Executors.newFixedThreadPool(3);
     }
 
     public void displayImage(final Cursor cursor, final ImageView imageView,final TextView txt){
+        final WeakReference<ImageView>imageViewReference=new WeakReference<ImageView>(imageView);
+        final WeakReference<TextView>textViewReference=new WeakReference<TextView>(txt);
         mExecutorService.submit(new Runnable() {
             @Override
             public void run() {
                 String title=cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.TITLE));
-                txt.setText(title);
+                if(textViewReference.get()!=null){
+                    textViewReference.get().setText(title);
+                }
+                //txt.setText(title);
                 int imageId=cursor.getInt(cursor.getColumnIndex(MediaStore.Images.Media._ID));
                 BitmapFactory.Options bmOptions = new BitmapFactory.Options();
                 bmOptions.inSampleSize =8;
@@ -33,12 +39,16 @@ public class ImageLoader {
                 final Bitmap map=MediaStore.Images.Thumbnails.getThumbnail(imageView.getContext().getContentResolver(),
                         imageId, MediaStore.Images.Thumbnails.MINI_KIND, bmOptions);
                 if(map!=null){
-                    imageView.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            imageView.setImageBitmap(map);
-                        }
-                    });
+                    final ImageView img=imageViewReference.get();
+                    if(img!=null){
+                        img.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                img.setImageBitmap(map);
+                            }
+                        });
+                    }
+
                 }
             }
         });
