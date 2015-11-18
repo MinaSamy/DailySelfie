28928c451e95a4213b19642ae47f3edccfad269b
Mina Samy <m.s.ramzy@gmail.com>
2015-11-18 13:46:27 +0200

    Show Capture Date

--- app/src/main/java/bloodstone/dailyselfie/android/utils/ImageLoader.java
+++ app/src/main/java/bloodstone/dailyselfie/android/utils/ImageLoader.java
@@ -8,6 +8,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,9 +18,13 @@ import java.util.concurrent.Executors;
  */
 public class ImageLoader {
     private ExecutorService mExecutorService;
+    private SimpleDateFormat mSdf;
+    private Calendar mCalendar;
 
     public ImageLoader(){
         mExecutorService= Executors.newFixedThreadPool(3);
+        mSdf=new SimpleDateFormat("EEE, MMM d, yyyy h:mm a");
+        mCalendar=Calendar.getInstance();
     }
 
     public void displayImage(final Cursor cursor, final ImageView imageView,final TextView txt){
@@ -27,7 +33,9 @@ public class ImageLoader {
         mExecutorService.submit(new Runnable() {
             @Override
             public void run() {
-                String title=cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.TITLE));
+                long captureDate=cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media.DATE_TAKEN));
+                mCalendar.setTimeInMillis(captureDate);
+                String title=mSdf.format(mCalendar.getTime());
                 if(textViewReference.get()!=null){
                     textViewReference.get().setText(title);
                 }
